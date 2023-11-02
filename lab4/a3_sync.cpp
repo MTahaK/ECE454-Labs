e18b1a9cff90b4047c07b579dd55e6d3b78759a2
MTahaK <taha.khokhar@mail.utoronto.ca>
2023-11-02 12:09:10 -0400

    Added Customer and Teller classes, along with basic serviceCustomer function

--- lab4/a3_sync.cpp
+++ lab4/a3_sync.cpp
@@ -2,8 +2,6 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
-#include <queue>
-#include <chrono>
 
 // Semaphore class to control access to the shared resources
 class Semaphore {
@@ -29,3 +27,43 @@ private:
     std::condition_variable condition;
     int count;
 };
+
+class Customer {
+public:
+    Customer(int id) : id(id), serviceTime(rand() % 30) {}
+
+    int id;
+    int serviceTime; // Random service time less than 30 seconds
+
+    // Set the thread servicing this customer
+    void setServiceThread(std::thread::id threadId) {
+        servicingThreadId = threadId;
+    }
+
+    // Get the thread id servicing this customer
+    std::thread::id getServiceThread() const {
+        return servicingThreadId;
+    }
+
+private:
+    std::thread::id servicingThreadId; // Stores the ID of the servicing thread
+};
+
+class Teller {
+public:
+    Teller() : isAvailable(true) {}
+
+    bool isAvailable;
+    // Additional teller-related methods and attributes could be here.
+};
+
+void serviceCustomer(Customer &customer, Teller &teller) {
+    // Service the customer
+    std::cout << "Teller " << std::this_thread::get_id() << " started serving customer " << customer.id << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(customer.serviceTime));
+    std::cout << "Teller " << std::this_thread::get_id() << " finished serving customer " << customer.id << std::endl;
+
+    // Set the teller to available
+    teller.isAvailable = true;
+
+}
