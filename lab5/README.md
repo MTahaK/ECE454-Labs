7e0e3c397c7198f765cbdb4f8d5fc81417905540
MTahaK <taha.khokhar@mail.utoronto.ca>
2023-11-30 05:22:54 -0500

    Calculated execution time, and added to new README.

---
+++ lab5/README.md
@@ -0,0 +1,25 @@
+# ECE454 A5
+By: Taha Khokhar, ID: 1005813069
+
+Group #30
+
+Lab Session 02
+
+Date: 2023-11-30
+
+# Lab 5:
+## Instructions for Launch and Configuration:
+Simply compile the program by running `g++ a5.cpp`.
+
+## Part C:
+### Execution time before optimization:
+Computed on the "Elden Ring" title, which occurred 14 times.
+
+| Run #       | Execution Time (s) | Execution Time (µs) |
+| ----------- | ------------------ | ------------------- |
+| 1           | 0.000226969        | 226.969             |
+| 2           | 0.000216689        | 216.689             |
+| 3           | 0.000237319        | 237.319             |
+| 4           | 0.000214019        | 214.019             |
+| 5           | 0.00021626         | 216.26              |
+| **Average** | 0.000222251        | 222.251             |
--- lab5/a5.cpp
+++ lab5/a5.cpp
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <list>
 #include <vector>
+#include <chrono>
 
 struct Occurrence {
     std::string title;
@@ -24,7 +25,10 @@ void process_file(const std::string& filename, std::unordered_map<std::string, O
         bool isTitle = false;
         std::string title;
 
+        // Parse line as string stream
         while (iss >> word) {
+            // If word starts with '<', it is a title; start adding words to title until
+            // end demarcated by '>'
             if (word.front() == '<') {
                 isTitle = true;
                 title = word;
@@ -33,7 +37,7 @@ void process_file(const std::string& filename, std::unordered_map<std::string, O
                 isTitle = false;
 
                 // If title had trailing punctuation after the closing angle bracket,
-                // remove it.
+                // remove it. Example: <Elden Ring>, -> <Elden Ring>
                 if(word.back() != '>') {
                     title = title.substr(0, title.size() - 1);
                 }
@@ -46,6 +50,7 @@ void process_file(const std::string& filename, std::unordered_map<std::string, O
                     hash_table[title] = new Occurrence(title, line);
                 } else {
                     Occurrence* newOccurrence = new Occurrence(title, line);
+                    // Initialize count to -1. 
                     newOccurrence->count = -1;
                     newOccurrence->next = hash_table[title];
                     hash_table[title] = newOccurrence;
@@ -76,9 +81,12 @@ void shuffle_hash_tables(std::vector<std::unordered_map<std::string, Occurrence*
     for (auto hash_table : hash_tables) {
         if (hash_table != hash_table1) {
             for (auto &pair : *hash_table){
+                // If title is not in hash_table1, add it
                 if (hash_table1->find(pair.first) == hash_table1->end()) {
                     (*hash_table1)[pair.first] = pair.second;
-                } else {
+                }
+                // Otherwise, append the list of occurrences to the end of the list in hash_table1 
+                else {
                     Occurrence* occ = (*hash_table1)[pair.first];
                     while (occ->next != nullptr) {
                         occ = occ->next;
@@ -97,69 +105,50 @@ int main() {
     std::vector<std::unordered_map<std::string, Occurrence*>*> hash_tables = {&hash_table1, &hash_table2, &hash_table3, &hash_table4};
     std::vector<std::string> files = {"machine1.txt", "machine2.txt", "machine3.txt", "machine4.txt"};
 
+    // Start timer
+    auto start = std::chrono::high_resolution_clock::now();
+    // Map files to hash tables
     map_files(hash_tables, files);
 
-    // std::cout<<"==============\nmachine1.txt\n==============\n\n";
-    // for (auto& pair : hash_table1) {
-    //     std::cout << "Title: " << pair.first << "\nOccurrences:\n";
-    //     for (Occurrence* occ = pair.second; occ != nullptr; occ = occ->next) {
-    //         std::cout << occ->line << "\n";
-    //     }
-    //     std::cout << "\n";
-    // }
-    // std::cout<<"==============\nmachine2.txt\n==============\n\n";
-    // for (auto& pair : hash_table2) {
-    //     std::cout << "Title: " << pair.first << "\nOccurrences:\n";
-    //     for (Occurrence* occ = pair.second; occ != nullptr; occ = occ->next) {
-    //         std::cout << occ->line << "\n";
-    //     }
-    //     std::cout << "\n";
-    // }
-    // std::cout<<"==============\nmachine3.txt\n==============\n\n";
-    // for (auto& pair : hash_table3) {
-    //     std::cout << "Title: " << pair.first << "\nOccurrences:\n";
-    //     for (Occurrence* occ = pair.second; occ != nullptr; occ = occ->next) {
-    //         std::cout << occ->line << "\n";
-    //     }
-    //     std::cout << "\n";
-    // }
-    // std::cout<<"==============\nmachine4.txt\n==============\n\n";
-    // for (auto& pair : hash_table4) {
-    //     std::cout << "Title: " << pair.first << "\nOccurrences:\n";
-    //     for (Occurrence* occ = pair.second; occ != nullptr; occ = occ->next) {
-    //         std::cout << occ->line << "\n";
-    //     }
-    //     std::cout << "\n";
-    // }
-
     // Amalgamate all hash tables into hash_table1
-
     shuffle_hash_tables(hash_tables);
+
     // Go through hash table; count occurrences, add new node to start of list that tracks count.
     for (auto& pair : hash_table1) {
         int count = 0;
         for (Occurrence* occ = pair.second; occ != nullptr; occ = occ->next) {
             count++;
         }
+        // Add new node to start of list that tracks count. Faster access to count.
         Occurrence* newOccurrence = new Occurrence(pair.first, "");
         newOccurrence->count = count;
         newOccurrence->next = pair.second;
         pair.second = newOccurrence;
     }
 
-    std::cout<<"==============\nAmalgamate of Hash Table 1, 2, 3, and 4\n==============\n\n";
-    for (auto& pair : hash_table1) {
-        std::cout << "\n============================\nTitle: " << pair.first <<std::endl;
-        if(pair.second->count != -1) {
-            std::cout << "Number of Occurrences: " << pair.second->count << std::endl;
-        }
-        std::cout << "Occurrences:\n";
-        for (Occurrence* occ = pair.second; occ != nullptr; occ = occ->next) {
-            std::cout << occ->line << "\n";
-        }
-        std::cout << "\n============================\n\n";
-    }
+    // Get the count of one of the titles
+    std::string title = "Elden Ring";
+    // Access hash table, get count from first node in pair.second list
+    int count = hash_table1[title]->count; 
+    std::cout << "Number of occurrences of " << title << ": " << count << std::endl;
+    // End timer
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
 
+    // std::cout<<"==============\nAmalgamate of Hash Table 1, 2, 3, and 4\n==============\n\n";
+    // for (auto& pair : hash_table1) {
+    //     std::cout << "\n============================\nTitle: " << pair.first <<std::endl;
+    //     if(pair.second->count != -1) {
+    //         std::cout << "Number of Occurrences: " << pair.second->count << std::endl;
+    //     }
+    //     std::cout << "Occurrences:\n";
+    //     for (Occurrence* occ = pair.second; occ != nullptr; occ = occ->next) {
+    //         std::cout << occ->line << "\n";
+    //     }
+    //     std::cout << "\n============================\n\n";
+    // }
+
+    std::cout << "Time taken: " << elapsed.count() << " seconds\n";
     // Free hash table memory
     for (const auto& pair : hash_table1) {
         Occurrence* occ = pair.second;
